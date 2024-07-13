f9b99e4ce3eae9538584ba4e2796e027a21dedc9
SangHun <shzero211@naver.com>
2024-07-13 17:01:26 +0900

    SpringBoot 프로젝트 생성

---
+++ src/main/java/com/ksh/springboot/StudyAllLibForSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.ksh.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyAllLibForSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyAllLibForSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ksh/springboot/StudyAllLibForSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ksh.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyAllLibForSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
